6dde143b26aa0f5ec1a953531dacd0e300fd9dfc
Ross Jameson <Rossjameson1623@gmail.com>
2019-10-30 19:18:35 -0500

    finished

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () =>{
+        return(
+            <header>
+                <i className="fas fa-book-dead fa-4x"></i>
+                <h1>Ross's Spooky time book nook</h1>
+            </header>
+            );
+          }
+
+export default Header;
